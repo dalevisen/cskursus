1bc32046d423d66a2b29a96d640dbdcb5ac658b6
admin <admin@TA-8211-01.kursdom.net>
2018-01-23 10:49:00 +0100

    Add project files.

---
+++ variabler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variabler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                int[] mløn = { 1, 2 };
+                               
+                Console.WriteLine(mløn[0]);
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
+    
+    
+
